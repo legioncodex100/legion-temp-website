d76519ac4000467ec740677f1b10643c08cdd1a0
mointhecloud <mohammed@twobrothers.tech>
2026-02-13 14:23:31 +0000

    feat: add bowtie.ai chat widget

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,6 +62,13 @@ export default function RootLayout({
                     strategy="afterInteractive"
                 />
 
+                {/* Bowtie.ai Chat Widget */}
+                <Script id="bowtie-widget" strategy="afterInteractive">
+                    {`
+                        (function(t,i,o,n,w,a,e){t["bowtieDataToken"]=w;t[w]=t[w]||function(){(t[w].i=t[w].i||[]).push(arguments)};t[w].o=1*new Date;a=i.createElement(o);e=i.getElementsByTagName(o)[0];a.async=1;a.src=n;e.parentNode.insertBefore(a,e)})(window,document,"script","https://s3.amazonaws.com/bowtie.ai/bowtieJS/bowtie_widget.js","legion_grappling_academy");
+                    `}
+                </Script>
+
                 <Header />
                 <main className="flex-grow">
                     {children}
